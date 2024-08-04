4f05fdafad3f59e50ccb9ea4919e90d5dfda50e4
lucas <lucashk.eng@gmail.com>
2024-08-04 20:40:25 -0300

    packages

---
+++ OAG/02_fundamentals/packages/auxiliary/auxiliary.go
@@ -0,0 +1,9 @@
+package auxiliary
+
+import "fmt"
+
+// Public functions must start with capital letter
+func Write() {
+	fmt.Println("Writing from auxiliary file")
+	write2()
+}
---
+++ OAG/02_fundamentals/packages/auxiliary/auxiliary2.go
@@ -0,0 +1,8 @@
+package auxiliary
+
+import "fmt"
+
+// Private functions must start with lowercase letter
+func write2() {
+	fmt.Println("Writing from auxiliary file 2")
+}
